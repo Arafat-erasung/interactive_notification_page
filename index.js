a7532804e489261fa591d1c9586f91166fa3541d
Arafat-erasung <arafaterasung@gmail.com>
2022-09-24 02:56:57 +0200

    added an event listener to all card element/divs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const markAllAsRead = document.getElementById("mark");
 const notificationCount = document.getElementById("notification-count");
-const notificationCard = document.getElementById("notification-card");
+const notificationCard = document.querySelectorAll(".notification-card");
 const bulletIcons = document.querySelectorAll("i")
 const directMsg = document.getElementsByClassName("dm");
 
 
 markAllAsRead.addEventListener('click', mark);
+
 
 function mark() {
     for (const bulletIcon of bulletIcons) {
@@ -15,5 +16,11 @@ function mark() {
 }
 
 
+
+for (let card of notificationCard){
+    card.addEventListener('click', function() {
+        console.log("clicked");
+    });
+}
 
 
